299532cfca8568dc22353aabeb4be35aeff19002
gabrielgomes3398 <g.g.d.coxa@gmail.com>
2025-02-17 23:03:41 +0000

    printf Código

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,10 +3,12 @@
 int main(){
    int População, Numeros de pontos túristicos; 
    float Área km², PIB;
-   char Estado, Código da Carta;
+   char Estado, Código;
 
    printf("Estado: \n");
    scanf("%s, &Estado")
+
+   printf("Código: \n");
    
    
 
