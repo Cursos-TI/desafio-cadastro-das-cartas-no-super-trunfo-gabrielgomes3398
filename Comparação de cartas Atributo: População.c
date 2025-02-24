f8e40f7b384e88784f0bb3100c9b469a2b86b09e
gabrielgomes3398 <g.g.d.coxa@gmail.com>
2025-02-24 23:30:59 +0000

    printf("Carta2 venceu!\n");

--- Comparação de cartas Atributo: População.c
+++ Comparação de cartas Atributo: População.c
@@ -5,16 +5,17 @@ int main(){
     int carta1 = 12300000;
     int carta2 = 6000000;
 
-    printf("Entre com a carta1: \n");
+    printf("Entre com a carta1 São Paulo: \n");
     scanf("%d", carta1);
 
-    printf("Entre com carta2: \n");
+    printf("Entre com carta2 Rio de Janeiro: \n");
     scanf("%d", carta2);
 
 
     if (carta1 > carta2){
         printf("São Paulo tem mais populção");
-    } else {   
+    } else { 
+        printf("Rio de Janeiro tem mais população")  
 
 
 
